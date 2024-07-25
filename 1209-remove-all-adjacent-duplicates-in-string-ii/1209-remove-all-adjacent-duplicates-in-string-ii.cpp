ff0b7035a707ed0c2b90f55c014df8b9d2672f69
Sushant Gupta <154793205+sushant0911@users.noreply.github.com>
2024-07-25 10:05:17 +0530

    Time: 13 ms (80.11%), Space: 12.8 MB (30.83%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack<pair<char,int>> st;
-        for(int i = 0 ; i < s.length() ; i++){
-            if(st.empty()) st.push({s[i],1});
-            else if(st.top().first == s[i]){
-                st.push({s[i],st.top().second +1});
+        stack<pair<char, int>> st;
+        for (char ch : s) {
+            if (!st.empty() && st.top().first == ch) {
+                st.top().second++;
+            } else {
+                st.push({ch, 1});
             }
-            else{
-                st.push({s[i],1});
-            }
-            if(st.top().second == k){
-                for(int i = 0 ; i < k ; i++){
-                    st.pop();
-                }
+            if (st.top().second == k) {
+                st.pop();
             }
         }
-        string ans = "";
-        while(!st.empty()){
-            ans += st.top().first;
+        string ans;
+        while (!st.empty()) {
+            ans.append(st.top().second, st.top().first);
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     string removeDuplicates(string s, int k) {
+//         stack<pair<char,int>> st;
+//         for(int i = 0 ; i < s.length() ; i++){
+//             if(st.empty()) st.push({s[i],1});
+//             else if(st.top().first == s[i]){
+//                 st.push({s[i],st.top().second +1});
+//             }
+//             else{
+//                 st.push({s[i],1});
+//             }
+//             if(st.top().second == k){
+//                 for(int i = 0 ; i < k ; i++){
+//                     st.pop();
+//                 }
+//             }
+//         }
+//         string ans = "";
+//         while(!st.empty()){
+//             ans += st.top().first;
+//             st.pop();
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
