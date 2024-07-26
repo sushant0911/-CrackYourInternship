1179ccbf8d8e0c04b1ad42b0e81c38ce04f2d05e
Sushant Gupta <154793205+sushant0911@users.noreply.github.com>
2024-07-26 21:04:05 +0530

    Time: 14 ms (9.06%), Space: 19.8 MB (32.55%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -11,32 +11,53 @@
  */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if(!root) return false;
-        stack<TreeNode*> path;
-        stack<int> pathSum;
-        path.push(root);
-        pathSum.push(root->val);
-        while(!path.empty()){
-            TreeNode* temp = path.top();
-            path.pop();
-            int tempVal = pathSum.top();
-            pathSum.pop();
+    bool hasPathSum(TreeNode* Node, int sum) {
+        if (Node == NULL)
+        return 0;
+ 
+    bool ans = 0;
+ 
+    int subSum = sum - Node->val;
+ 
+    /* If we reach a leaf node and sum becomes 0 then return
+     * true*/
+    if (subSum == 0 && Node->left == NULL
+        && Node->right == NULL)
+        return 1;
+ 
+    /* otherwise check both subtrees */
+    if (Node->left)
+        ans = ans || hasPathSum(Node->left, subSum);
+    if (Node->right)
+        ans = ans || hasPathSum(Node->right, subSum);
+ 
+    return ans;
+        
+        //         if(!root) return false;
+//         stack<TreeNode*> path;
+//         stack<int> pathSum;
+//         path.push(root);
+//         pathSum.push(root->val);
+//         while(!path.empty()){
+//             TreeNode* temp = path.top();
+//             path.pop();
+//             int tempVal = pathSum.top();
+//             pathSum.pop();
             
-            if(temp->left == nullptr && temp->right == nullptr && tempVal == targetSum){
-                return true;
-            }
+//             if(temp->left == nullptr && temp->right == nullptr && tempVal == targetSum){
+//                 return true;
+//             }
             
-            if(temp->right != nullptr){
-                path.push(temp->right);
-                pathSum.push(temp->right->val + tempVal);
-            }
+//             if(temp->right != nullptr){
+//                 path.push(temp->right);
+//                 pathSum.push(temp->right->val + tempVal);
+//             }
             
-            if(temp->left != nullptr){
-                path.push(temp->left);
-                pathSum.push(temp->left->val + tempVal);
-            }
-        }
-        return false;
+//             if(temp->left != nullptr){
+//                 path.push(temp->left);
+//                 pathSum.push(temp->left->val + tempVal);
+//             }
+//         }
+//         return false;
     }
 };
