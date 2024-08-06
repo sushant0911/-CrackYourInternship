f5e25f1f389c348ae65345adf331f8b8a2db63c3
Sushant Gupta <154793205+sushant0911@users.noreply.github.com>
2024-08-06 11:47:50 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (6.17%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
-    int numTrees(int n) {
-        if(n == 0 || n == 1) return 1;
+    int solve(int n, vector<int>& dp){
+        if(n <= 1) return 1;
+        if(dp[n] != -1) return dp[n];
         int ans = 0;
+//         Think i as a  root node
         for(int i = 1 ; i <= n ; i++){
-            ans += numTrees(i - 1) * numTrees(n - i);
+            ans += solve(i - 1, dp) * solve(n - i, dp);
         }
-        return ans;
+        return dp[n] = ans;
+    }
+    
+    int numTrees(int n) {
+        // Recursion
+        
+        // if(n == 0 || n == 1) return 1;
+        // int ans = 0;
+        // for(int i = 1 ; i <= n ; i++){
+        //     ans += numTrees(i - 1) * numTrees(n - i);
+        // }
+        // return ans;
+        
+        // Recur + Memo
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
